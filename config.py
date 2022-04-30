b2f6502f22e1d5d115615c0c1af38b993c9cbf01
Alidar Asvarov <alidar.asvarov@gmail.com>
2022-04-30 16:00:17 +0300

    Add VAD_Classifier abstract class
    To test quality of different voice activity detectors

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
 import yaml
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 config = yaml.safe_load(open("config.yaml", "rb"))
@@ -6,3 +11,6 @@ config['frame_size'] = int(config['sample_rate'] * (config['frame_size_ms'] / 10
 
 if config['wandb']['name'] == 'as_model':
     config['wandb']['name'] = config['model']
+
+config["cobra_access_key"] = os.environ['COBRA_ACCESS_KEY']
+
--- utils/initialization.py
+++ utils/initialization.py
@@ -1,16 +1,11 @@
 import os
-import random
 
 import torch
-import numpy as np
+import pytorch_lightning
 
 
 def seed_everything(seed=42):
-    random.seed(seed)
+    pytorch_lightning.seed_everything(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
